0bceccfe6539ae890836b491030fa71f32759632
yudlab <47621851+yudlab@users.noreply.github.com>
2021-01-26 16:13:12 +0400

    Add odo

--- speedo.ino
+++ speedo.ino
@@ -10,29 +10,30 @@
 #include <ArduinoJson.h>
 
 int braudRate = 9600;
-int debounceRate = 100; //in ms
-int refreshRate = 500; //in ms
-float wheelCirc = 1.93; // wheel circumference (in meters)
-
-// constants won't change. Used here to set a pin number:
-const int ledPin =  LED_BUILTIN;// the number of the LED pin
-
-// Variables will change:
-int ledState = LOW; // ledState used to set the LED
-unsigned long previousMillis = 0; // will store last time LED was updated
-const long interval = 600; // ms
+int debounceRate = 300; //in ms
+float wheelCirc = 0.00193; // wheel circumference (in km)
 
 uint8_t HALL_SENSOR = D2; // Pin where the hall sensor is connected to
+uint8_t CLEAR_ODO_BTN =  D1; //Pin to reset odo
 const char* configFile = "config.odo"; //file containing the configuration settings
 
-File SDCon;
 String speedoSDData;
+DynamicJsonBuffer jsonBuffer;
+File SDCon;
+char buffer[200];
 
-float distanceKM, speedp, start, speedk, lastRev, lastRefresh;
-bool updateSD = true;
-int elapsed;
+int ledState = LOW; // ledState used to set the LED
+unsigned long previousMillis = 0; // will store last time LED was updated
+const long blinkInterval = 700; // ms
+bool updateSD = false;
+
+
+float odo, trip, previousSpeed, start, currentSpeed, tLastRev, tLastRefresh, tLastReset;
+int tElapsed;
+
 
 void ICACHE_RAM_ATTR speedCalc ();
+void ICACHE_RAM_ATTR resetOdo ();
 
 void setup ()
   {
@@ -45,68 +46,75 @@ void setup ()
     }
 
     if(!getConfig()) {
-      distanceKM = 0;
-      updateSD = false;
+      odo = 0;
+      trip= 0;
+      blink(blinkInterval);
+    } else {
+      updateSD = true;
     }
     //toggle function speedCalc when HALL Sensor closes
-    attachInterrupt(digitalPinToInterrupt(HALL_SENSOR), speedCalc, RISING); // interrupt called when sensors sends digital 2 high (every wheel rotation)
+    attachInterrupt(digitalPinToInterrupt(HALL_SENSOR), speedCalc, RISING);
+    attachInterrupt(digitalPinToInterrupt(CLEAR_ODO_BTN), resetOdo, RISING);
 
     //start now (it will be reset by the interrupt after calculating revolution time)
     start = millis(); 
 
     // Print a transitory message to the LCD.
     Serial.println("Hello Yudish.");
-    delay(5000); //just to allow you to read the initialization message
+    delay(3000); //just to allow you to read the initialization message
   }
-
-int blink ()
+void loop()
   {
-    unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= interval) {
-      // save the last time you blinked the LED
-      previousMillis = currentMillis;
-
-      // if the LED is off turn it on and vice-versa:
-      if (ledState == LOW) {
-        ledState = HIGH;
-      } else {
-        ledState = LOW;
-      }
-
-      // set the LED with the ledState of the variable:
-      digitalWrite(LED_BUILTIN, ledState);
+    //set speed to zero if HALL Sensor open for more than 2s
+    if ( millis() - tLastRev > 2000 ){
+      currentSpeed = 0;
     }
-  }
+    if ( updateSD && currentSpeed != previousSpeed &&  millis() - tLastRefresh > 10000 ) {
+      updateDistanceSD();    
+      tLastRefresh = millis();
+    }
 
+    
+    Serial.println();
+    Serial.print(currentSpeed);
+    Serial.print("KM/H  | TRIP: ");
+    Serial.print(trip);
+    Serial.print("KM | ODO: ");
+    Serial.print(odo);
+    Serial.print("KM");
+    Serial.println();
+    delay(900);
+  }
 bool getConfig ()
   {
-    StaticJsonBuffer<200> jsonBuffer;
     Serial.println();
-    Serial.println("Connecting to SD:");
+    Serial.println("Connecting to SD(getConfig):");
     if (!SD.begin(4)) {
-      Serial.println("SD Error: 1");
+      Serial.println("SD Connection error(state 4)");
       return false;
     }
     Serial.println("SD Connected.");
 
     SDCon = SD.open(configFile);
     if (SDCon) {
-      int i =0;
+      int i = 0;
       while (SDCon.available()) {
         char c=SDCon.read();
         if (c=='}'){
           speedoSDData.concat(c);
-          //Serial.println(speedoSDData);
-            JsonObject& root = jsonBuffer.parseObject(speedoSDData);
-            if (!root.success()) {
-              Serial.println("parseObject() failed");
-              return false;
-            }
-          
-          distanceKM = root["distance"];
-          Serial.print("Distance was set to: ");
-          Serial.println(distanceKM);
-
+          JsonObject& root = jsonBuffer.parseObject(speedoSDData);
+          if (!root.success()) {
+            Serial.println("JSON INVALID/PARSE ERROR");
+            Serial.println("Contents: ");
+            Serial.println(speedoSDData);
+            return false;
+          }
+          odo = root["odo"];
+          trip = root["trip"];
+          Serial.print("ODO: ");
+          Serial.println(odo);
+          Serial.print("TRIP: ");
+          Serial.println(trip);
         }
         speedoSDData.concat(c);
       }
@@ -120,29 +128,25 @@ bool getConfig ()
   }
 bool updateDistanceSD ()
   {
-    StaticJsonBuffer<200> jsonBuffer;
-    speedp = distanceKM;
-    Serial.println("Connecting to SD:");
-    if (!SD.begin(4)) {
-      Serial.println("SD Error: 1");
-      return false;
-    }
-    Serial.println("SD Connected.");
+    previousSpeed = currentSpeed;
     SD.remove(configFile);
 
     SDCon = SD.open(configFile, FILE_WRITE);
     if (SDCon) {
-      char buffer[200];
+      DynamicJsonBuffer jsonBuffer;
       JsonObject& dist = jsonBuffer.createObject();
-      dist["distance"] = distanceKM;
-      Serial.print("dst: ");
-      Serial.println(distanceKM);
-      Serial.println("___________");
+      if (!dist.success()) {
+        Serial.println("ERROR CREATING JSON");
+        return false;
+      }
+
+      dist["odo"] = odo;
+      dist["trip"] = trip;
       dist.prettyPrintTo(buffer);
       SDCon.print(buffer);
-      SDCon.close();
-      Serial.print("Updated dist: ");
+      Serial.println("SD Updated: ");
       Serial.println(buffer);
+      SDCon.close();
       return true;
     }
     else {
@@ -152,31 +156,42 @@ bool updateDistanceSD ()
   }
 void speedCalc ()
   { //Function called by the interrupt
-
-    distanceKM = distanceKM + (wheelCirc/1000);
-    lastRev = millis();
-
+    blink(blinkInterval);
+    tLastRev = millis();
     if ( ( millis() - start ) > debounceRate )
-      {
-          elapsed = millis() - start;
-          start = millis();
-          speedk = ( 3600 * wheelCirc ) / elapsed;
-      }
+    {
+        tElapsed = millis() - start;
+        start = millis();
+        currentSpeed = ( 3600000 * wheelCirc ) / tElapsed;
+    }
+
+    odo = odo + wheelCirc;
+    trip = trip + wheelCirc;
+
   }
-void loop()
+void resetOdo ()
   {
-    //set speed to zero if HALL Sensor open for more than 2s
-    if ( millis() - lastRev > 2000 ){
-      speedk = 0;
+    if(trip != 0 && millis() - tLastReset > 3000 ){
+      trip = 0;
+      updateDistanceSD();
+      tLastReset = millis();
     }
-    if ( updateSD && speedk != speedp &&  millis() - lastRefresh > 15000 ) {
-      updateDistanceSD();    
-      lastRefresh = millis();
+  }
+int blink (int blinkInterval)
+  {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= blinkInterval) {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
+
+      // if the LED is off turn it on and vice-versa:
+      if (ledState == LOW) {
+        ledState = HIGH;
+      } else {
+        ledState = LOW;
+      }
+
+      // set the LED with the ledState of the variable:
+      digitalWrite(LED_BUILTIN, ledState);
     }
-    Serial.print(int(speedk));
-    Serial.println(" km/h");
-    Serial.print(distanceKM);
-    Serial.println(" km");
-    delay(refreshRate);
-    blink();
   }
